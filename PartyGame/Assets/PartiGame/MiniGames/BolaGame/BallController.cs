7e8fe2059ba93e3cd25f45914858fd1ad0aebb30
Marçal <cimarin67@gmail.com>
2020-04-28 21:00:42 +0200

    BolaGameScene

---
+++ PartyGame/Assets/PartiGame/MiniGames/BolaGame/BallController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    bool movable;
+
+    public float ballSpeed;
+    public GameObject Sphere;
+    public Vector3 scale;
+
+    void Start()
+    {
+        scale = Sphere.transform.localScale;
+    }
+
+    void Update()
+    {
+
+        Move();
+
+    }
+  
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Campo"))
+        {
+            movable = true;
+        }
+        else { movable = false; };
+
+      
+    }
+
+    void Move()
+    {
+        if(movable == true)
+        {
+            float xSpeed = Input.GetAxis("Horizontal");
+            float ySpeed = Input.GetAxis("Vertical");
+
+
+            Rigidbody body = GetComponent<Rigidbody>();
+            body.AddTorque(new Vector3(xSpeed, 0, ySpeed) * ballSpeed * Time.deltaTime * 2);
+
+
+        }
+    }
+    
+}
